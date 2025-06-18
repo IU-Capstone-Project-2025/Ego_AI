19bd014d87ffd03c3720520592b3720f6929c286
setterwars <s.zaynulin@innopolis.university>
2025-06-18 11:43:59 +0300

    Edited Architecture of dockerfile

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { General } from "./screens/General/General";
+import OtherPage from "./screens/OtherPage";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <General />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<General />} />
+        <Route path="/other" element={<OtherPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
